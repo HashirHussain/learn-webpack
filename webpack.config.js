99e1426e9364f959f1a1f40367ebd431bfee2be0
Hashir Hussain <hashir.hussain@yash.com>
2021-05-26 19:56:30 +0530

    Add custom entry and and output path in config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,13 @@
-module.exports = {};
+const path = require("path");
+
+module.exports = {
+  //by-default webpack look the entry file inside src/index.js,
+  //but we can change the entry whatever we want.
+  entry: "./app/app.js",
+  output: {
+    //by-default webpack puts all its bundle into dist/main.js,
+    //but we can customize the folder and the file name.
+    filename: "myBundle.js",
+    path: path.resolve(__dirname, "bundle"),
+  },
+};
